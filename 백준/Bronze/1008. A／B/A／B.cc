f38dfb57039ee3f6c9bdec671497134c985f473b
hefxpzwk <hefxpzwk@gmail.com>
2025-10-09 06:10:08 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    
+    int a,b;
+    scanf("%d %d",&a,&b);
+    double answer = double(a)/b;
+    printf("%.9lf",answer);
+    
+    
+    
+    return 0;
+}
