c07e0596feb0b7ac329232148a62677f3361a755
hefxpzwk <hefxpzwk@gmail.com>
2025-10-09 06:23:06 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    
+    cout<<(a*a+b*b+c*c+d*d+e*e)%10;
+    
+    
+    return 0;
+}
