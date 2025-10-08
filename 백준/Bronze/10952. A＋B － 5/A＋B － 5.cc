1746d1d39a7c7cf78ec2b0e7b07880274e8ceff1
hefxpzwk <hefxpzwk@gmail.com>
2025-10-09 06:35:20 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a,b;
+    while(cin>>a>>b) {
+        if(a!=0 && b!=0)
+            cout<<a+b<<"\n";
+    }
+    
+    return 0;
+}
