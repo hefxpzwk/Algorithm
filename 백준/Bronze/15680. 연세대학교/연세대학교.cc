d99174239e18b02ccd31390619636654615b57b8
hefxpzwk <hefxpzwk@gmail.com>
2025-10-09 18:06:58 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin>>n;
+    
+    if(n==0) {
+        cout<<"YONSEI";
+    }
+    else {
+        cout<<"Leading the Way to the Future";
+    }
+    return 0;
+}
