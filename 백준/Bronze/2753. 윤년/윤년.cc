76920d65ec62b0e2588f9dc95c8acd51775a17b0
hefxpzwk <hefxpzwk@gmail.com>
2025-10-09 18:03:50 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int year;
+    cin>>year;
+    
+    cout<<(((year%4==0 && year%100!=0) || (year%400==0)) ? 1 : 0); 
+    
+    return 0;
+}
