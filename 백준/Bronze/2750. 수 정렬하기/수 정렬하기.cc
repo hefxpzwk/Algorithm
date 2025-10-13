bb0bb4d69ee5a52c8f56dbaf1b74ad2fa9289031
hefxpzwk <hefxpzwk@gmail.com>
2025-10-14 08:31:11 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t;
+    cin>>t;
+    
+    
+    vector<int> vec;
+    for(int i=0;i<t;i++) {
+        int a;
+        cin>>a;
+        vec.push_back(a);
+    }
+    
+    sort(vec.begin(), vec.end());
+    
+    for(auto i : vec) {
+        cout<<i<<"\n";
+    }
+    
+    return 0;
+}
