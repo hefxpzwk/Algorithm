4d85dca837aa13eb8f71997f390b724bdbb6766d
hefxpzwk <hefxpzwk@gmail.com>
2025-10-14 12:01:50 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin>>n;
+    
+    vector<long> vec;
+    vec.push_back(0); //0
+    vec.push_back(1); //1
+    vec.push_back(1); //2
+    vec.push_back(2); //3
+    
+    int answer = 0;
+    
+    for(int i=4;i<=n;i++) {
+        vec.push_back(vec[i-1]+vec[i-2]);
+    }
+    
+    cout<<vec[n];
+    
+    
+    
+    
+    return 0;
+}
