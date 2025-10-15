a5a60ab806a355547ade96ff88f4ad6d05c81f43
hefxpzwk <hefxpzwk@gmail.com>
2025-10-15 16:13:15 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin>>n;
+    
+    if(n%2==0) cout<<"CY";
+    else cout<<"SK";
+    
+    return 0;
+}
